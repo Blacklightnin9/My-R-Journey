8edc7daa43ffe294ea70664fe17d78cf31603e9b
Blacklightnin9 <maingame0911@gmail.com>
2025-04-26 11:54:58 +0800

    commenting for easier read

--- R-Basics/code.R
+++ R-Basics/code.R
@@ -1,1 +1,9 @@
+# installing the tidyverse package
+install.packages("tidyverse")
+# loading the tidyverse package into the R session
 library(tidyverse)
+
+# installing the dslabs package
+install.packages("dslabs")
+# loading the dslabs package into the R session
+library(dslabs)
